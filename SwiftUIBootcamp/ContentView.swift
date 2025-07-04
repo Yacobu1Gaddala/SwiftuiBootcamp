7c0267386ebc4ecaa303d649ae0b094fb085d420
Gaddala Yacobu <126049931+Yacobujet2022@users.noreply.github.com>
2025-07-04 14:33:05 +0530

    username component added

--- SwiftUIBootcamp/ContentView.swift
+++ SwiftUIBootcamp/ContentView.swift
@@ -8,19 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var userName: String = ""
     var body: some View {
         ZStack {
             Color.blue.opacity(0.5)
                 .ignoresSafeArea()
             
             VStack {
+                HStack {
+                    Text("Username")
+                        .font(.title)
+                        .fontWeight(.light)
+                    Spacer()
+                    TextField("Enter Username", text: $userName)
+                        
+                }
+                .padding()
                 
             }
-            .frame(width: 400,height: 300)
+            .frame(width: 500,height: 450)
             .background(Color.accentColor)
             .padding()
             .offset(x: 200,y: -100)
             .padding(.trailing,32)
+            .cornerRadius(8)
         }
     }
 }
